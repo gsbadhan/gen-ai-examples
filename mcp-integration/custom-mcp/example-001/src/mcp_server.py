9854569235657a3c4953de6185a81c7956213acc
gurpreet singh <gurpreetsingh@gurpreets-MacBook-Pro.local>
2025-09-04 14:58:09 +0530

    discount tool added

--- mcp-integration/custom-mcp/example-001/src/mcp_server.py
+++ mcp-integration/custom-mcp/example-001/src/mcp_server.py
@@ -12,13 +12,35 @@ product_catalog: dict = {
     "prd_004":{"name":"Stuffed cheesy garlic bread", "category":"sides", "base_price":160},
 }
 
+# product discounts
+product_discount: dict = {
+    "prd_001":{"name":"daily_discount","rate":"10", "unit":"percentage", "start_date":"", "end_date":""},
+    "prd_002":{"name":"buy500_discount","rate":"20", "unit":"price", "start_date":"", "end_date":""},
+}
+
+
 # get product detail
 @mcpServer.tool(name="product_detail")
 async def get_product_detail(product_id: str) -> dict:
     """ Get product detail by id """
     print(f"request for product_id {product_id}")
+    product = product_catalog.get(product_id)
+    if not product:
+        print(f"product_id not {product_id}")
+        return {}
+    return product
 
-    return product_catalog.get(product_id)
+
+# discounts for product
+@mcpServer.tool(name="discounts")
+async def get_discounts_of_product(product_id: str) -> dict:
+    """ Get discount detail for a product"""
+    print(f"request for product_id {product_id}")
+    discount = product_discount.get(product_id)
+    if not discount:
+        print(f"product_id not {product_id}")
+        return {}
+    return discount 
 
 
 # run MCP server
